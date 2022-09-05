539cf071692be8aa7a6718ac85db2db9cf940dcc
dcm@uns <38795567+dcm-uns@users.noreply.github.com>
2022-09-05 15:45:41 -0300

    features added

--- js/featureLoad.js
+++ js/featureLoad.js
@@ -11,4 +11,32 @@ fetch('js/feature2.js')
       console.log(text);
       eval(text)});
               
-  
+ 
+fetch('js/feature3.js')
+.then((response)=>response.text())
+.then((text)=>{
+  console.log(text);
+  eval(text)});
+          
+
+  fetch('js/feature4.js')
+  .then((response)=>response.text())
+  .then((text)=>{
+    console.log(text);
+    eval(text)});
+            
+
+    fetch('js/feature5.js')
+    .then((response)=>response.text())
+    .then((text)=>{
+      console.log(text);
+      eval(text)});
+              
+ 
+fetch('js/feature6.js')
+.then((response)=>response.text())
+.then((text)=>{
+  console.log(text);
+  eval(text)});
+          
+
